cacc63ac2a394a89f84f9992f9ed4d4c2d33474a
kimchankyo <51008440+kimchankyo@users.noreply.github.com>
2022-03-20 13:37:33 -0700

    Testing Links

---
+++ src/Website.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import App from "./App";
+
+class Website extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Website
+      </div>
+    );
+  }
+}
+
+export default Website;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
+import Website from './Website';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/s" element={<App/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
